47db7a809548bf2318b15d18f4537a6859eda974
BEASTDK7 <72353569+BEASTDK7@users.noreply.github.com>
2021-11-06 00:51:13 +0300

    Add files via upload

---
+++ lw_1/src/lw_1/Starter.java
@@ -0,0 +1,14 @@
+package lw_1;
+
+public class Starter {
+
+	public static void main(String[] args) {
+		// При использовании статического метода
+		//MyTask.DoTask();
+		
+		//При использовании методов уровня объекта
+		MyTask2 ob = new MyTask2();
+		ob.InputArray();
+		ob.PrintResult();
+	}
+}
